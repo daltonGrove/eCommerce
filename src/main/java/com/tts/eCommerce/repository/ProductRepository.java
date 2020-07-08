f4e0e58ca65b6880280376443802c4cdf784c191
daltonGrove <grovedalton@yahoo.com>
2020-07-08 10:04:18 -0400

    changes before break today

--- src/main/java/com/tts/eCommerce/repository/ProductRepository.java
+++ src/main/java/com/tts/eCommerce/repository/ProductRepository.java
@@ -1,5 +1,9 @@
 package com.tts.eCommerce.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +11,17 @@ import com.tts.eCommerce.model.Product;
 
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
+	
+	public List<Product> findAll();
+	public Optional<Product> findById(Long id);
+	public List<Product> findByBrand(String brand);
+	public List<Product> findByCategory(String category);
+	public List<Product> findByBrandAndCategory(String brand, String category);
+
+	@Query("SELECT DISTINCT product.brand FROM Product product")
+	public List<String> findDistinctBrands();
+
+	@Query("SELECT DISTINCT product.category FROM Product product")
+	public List<String> findDistinctCategories();
 
 }
