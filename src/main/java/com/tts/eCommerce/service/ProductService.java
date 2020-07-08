d5b6f8affe0747c1a65485f4cb2804d67b9975b5
DanielVera-ec <danieelveera@outlook.com>
2020-07-08 17:53:18 -0400

    Product Service

--- src/main/java/com/tts/eCommerce/service/ProductService.java
+++ src/main/java/com/tts/eCommerce/service/ProductService.java
@@ -1,5 +1,12 @@
 package com.tts.eCommerce.service;
 
 public class ProductService {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	public void save(Product product) {
+	      productRepository.save(product);
+	    }
 
 }
