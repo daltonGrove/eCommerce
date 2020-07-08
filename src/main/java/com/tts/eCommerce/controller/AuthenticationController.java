eb747a763564eb0821ff16e8552957206ec6a7e1
daltonGrove <grovedalton@yahoo.com>
2020-07-07 21:44:59 -0400

    added authentication controller code

--- src/main/java/com/tts/eCommerce/controller/AuthenticationController.java
+++ src/main/java/com/tts/eCommerce/controller/AuthenticationController.java
@@ -1,5 +1,46 @@
 package com.tts.eCommerce.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.tts.eCommerce.model.User;
+import com.tts.eCommerce.service.UserService;
+
+@Controller
 public class AuthenticationController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping(value = "/login")
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping(value = "/signup")
+	public String signup(Model model) {
+		User user = new User();
+		model.addAttribute("user", user);
+		return "signup";
+	}
+	
+	@PostMapping(value = "/signup")
+	public String createNewUser(@Valid User user, BindingResult bindingResult, Model model) {
+		User userExists = userService.findByUsername(user.getUsername());
+		if (userExists != null) {
+			bindingResult.rejectValue("username", "error.user", "Username is already taken");
+		} if (!bindingResult.hasErrors()) {
+			userService.saveNewUser(user);
+			model.addAttribute("success", "Sign up was Successful!");
+			model.addAttribute("user", new User());
+		}
+		return "signup";
+	}
 
 }
