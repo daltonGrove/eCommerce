166f69b83d29e2e19751eca810ddf98c50b7ee53
daltonGrove <grovedalton@yahoo.com>
2020-07-09 12:02:47 -0400

    changes from class

--- src/main/java/com/tts/eCommerce/controller/OrderController.java
+++ src/main/java/com/tts/eCommerce/controller/OrderController.java
@@ -1,5 +1,25 @@
 package com.tts.eCommerce.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.tts.eCommerce.model.Cart;
+import com.tts.eCommerce.model.Order;
+import com.tts.eCommerce.service.OrderService;
+
+@Controller
 public class OrderController {
+	
+	@Autowired
+	private OrderService orderService;
+	
+	@GetMapping("/checkout")
+	public String checkout(Cart cart, Model model) {
+		Order purchaseOrder = orderService.makePurchaseOrder(cart);
+		model.addAttribute("purchaseOrder", purchaseOrder);
+		return "storefront/invoice";
+	}
 
 }
--- src/main/java/com/tts/eCommerce/service/OrderService.java
+++ src/main/java/com/tts/eCommerce/service/OrderService.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tts.eCommerce.model.Cart;
 import com.tts.eCommerce.model.Order;
 import com.tts.eCommerce.repository.OrderRepository;
 
@@ -14,21 +15,39 @@ public class OrderService {
 	@Autowired
 	private OrderRepository orderRepository;
 	
-	public Order findByOrder (String order) {
-		return orderRepository.findByOrder(order);
-	}
-	
-	public Iterable<Order> getAllOrders(){
-		return orderRepository.findAll();
-	}
-	
-	public Order create(Order order) {
-		order.setCreatedAt(new Date());
-		return this.orderRepository.save(order);
-	}
-	
-	public void update(Order order) {
-		this.orderRepository.save(order);
-	}
+//	public Order findByOrder (String order) {
+//		return orderRepository.findByOrder(order);
+//	}
+//	
+//	public Iterable<Order> getAllOrders(){
+//		return orderRepository.findAll();
+//	}
+//	
+//	public Order create(Order order) {
+//		order.setCreatedAt(new Date());
+//		return this.orderRepository.save(order);
+//	}
+//	
+//	public void update(Order order) {
+//		this.orderRepository.save(order);
+//	}
 
+	
+	
+	//purpose of this method to create the order
+	public Order makePurchaseOrder(Cart cart) {
+		Order thatObjectThatBecomesAnOrderFromACart = new Order();
+		
+		// code to populate fields to create an order object. fields that are in order.java, fields for logged in user, fields to calculate the tax
+		return thatObjectThatBecomesAnOrderFromACart;
+	}
+	
+	
+	private Double calcSalesTax (Double subTotal) {
+		
+		// math for tax calculation. Look at documentation for this?
+		return tax;
+	}
+	
+	
 }
